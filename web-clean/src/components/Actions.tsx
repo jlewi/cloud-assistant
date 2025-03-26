230cb7483dd2d418c63c25c05723d9706c78a9f7
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-03-26 13:50:42 -0700

    Don't rerender console unless commands change
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- web-clean/src/components/Actions.tsx
+++ web-clean/src/components/Actions.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { memo, useState } from "react";
 import { Box, Button, Card } from "@radix-ui/themes";
 
 import Editor from "@monaco-editor/react";
@@ -9,9 +9,9 @@ type props = {
   title: string;
 };
 
-function RunActionButton({ exitCode, handleRunClick }: { exitCode: number | null, handleRunClick: () => void }) {
+function RunActionButton({ exitCode, onClick }: { exitCode: number | null, onClick: () => void }) {
   return (
-    <Button variant="soft" onClick={handleRunClick}>
+    <Button variant="soft" onClick={onClick}>
       {exitCode === null && (
         <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M3.24182 2.32181C3.3919 2.23132 3.5784 2.22601 3.73338 2.30781L12.7334 7.05781C12.8974 7.14436 13 7.31457 13 7.5C13 7.68543 12.8974 7.85564 12.7334 7.94219L3.73338 12.6922C3.5784 12.774 3.3919 12.7687 3.24182 12.6782C3.09175 12.5877 3 12.4252 3 12.25V2.75C3 2.57476 3.09175 2.4123 3.24182 2.32181ZM4 3.57925V11.4207L11.4338 7.5L4 3.57925Z" fill="currentColor" fillRule="evenodd" clipRule="evenodd" stroke="currentColor" strokeWidth="0.5"></path>
@@ -37,7 +37,7 @@ function RunActionButton({ exitCode, handleRunClick }: { exitCode: number | null
 // Action is an editor and an optional Runme console
 function Action({ value, title }: props) {
   let editorValue = value;
-  const [execCommands, setExecCommands] = useState<string[] | null>(null);
+  const [execCommands, setExecCommands] = useState<string[]>([]);
   const [exitCode, setExitCode] = useState<number | null>(null);
 
   const handleRunClick = () => {
@@ -57,11 +57,21 @@ function Action({ value, title }: props) {
     output = '';
   };
 
+  const MemoizedConsole = memo(({ commands }: { commands: string[] }) => (
+    <Console
+      rows={10}
+      commands={commands}
+      onStdout={outputHandler}
+      onStderr={outputHandler}
+      onExitCode={exitCodeHandler}
+    />
+  ), (prevProps, nextProps) => JSON.stringify(prevProps.commands) === JSON.stringify(nextProps.commands));
+
   return (
     <div>
       <Box className="w-full p-2">
         <div className="flex justify-between items-top">
-          <RunActionButton exitCode={exitCode} handleRunClick={handleRunClick} />
+          <RunActionButton exitCode={exitCode} onClick={handleRunClick} />
           <Card className="whitespace-nowrap overflow-hidden flex-1 ml-2">
             <div className="flex items-center m-1">
               <span>{title}</span>
@@ -76,17 +86,11 @@ function Action({ value, title }: props) {
                 onChange={(value) => editorValue = value || ""}
               />
             </div>
-            <div className="p-1">
-              {execCommands && (
-                <Console
-                  rows={10}
-                  commands={execCommands}
-                  onStdout={outputHandler}
-                  onStderr={outputHandler}
-                  onExitCode={exitCodeHandler}
-                />
-              )}
-            </div>
+            {execCommands.length > 0 && (
+              <div className="p-1">
+                <MemoizedConsole commands={execCommands} />
+              </div>
+            )}
           </Card>
         </div>
       </Box>
