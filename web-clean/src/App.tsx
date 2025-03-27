1545bae5852bb6c7b269ab53474308a52bd10a63
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-03-27 12:16:47 -0700

    Rudimentary Chat UX
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- web-clean/src/App.tsx
+++ web-clean/src/App.tsx
@@ -1,13 +1,14 @@
 import Layout from './layout'
 import { Helmet } from 'react-helmet'
 import { Theme } from '@radix-ui/themes'
+import '@radix-ui/themes/styles.css'
 
 import Actions from './components/Actions'
-import Chat from './components/Placeholder'
+import Chat from './components/Chat/Chat'
 import FileViewer from './components/Placeholder'
 
 import openaiLogo from './assets/openai.svg'
-import '@radix-ui/themes/styles.css'
+import { AgentClientProvider } from './contexts/AgentContext'
 
 function App() {
   return (
@@ -18,7 +19,9 @@ function App() {
           <meta name="description" content="An AI Assistant For Your Cloud" />
           <link rel="icon" href={openaiLogo} />
         </Helmet>
-        <Layout left={<Chat />} middle={<Actions />} right={<FileViewer />} />
+        <AgentClientProvider>
+          <Layout left={<Chat />} middle={<Actions />} right={<FileViewer />} />
+        </AgentClientProvider>
       </Theme>
     </>
   )
