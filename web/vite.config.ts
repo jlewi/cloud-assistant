1ac837367a18627c8cdf9a0047c5572535144db5
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-04-04 09:27:36 -0700

    New dist target to allow optional inclusion of assets
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,9 +2,19 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
+// import viteCompression from 'vite-plugin-compression'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    // viteCompression({
+    //   algorithm: 'gzip',
+    //   ext: '.gz',
+    //   deleteOriginFile: false,
+    // }),
+  ],
   build: {
     chunkSizeWarningLimit: 999999,
     rollupOptions: {
