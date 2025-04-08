273e3b379a55bd48c40c19b30db96cd939a05b5e
Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>
2025-04-08 13:06:11 -0700

    First version of Settings
    Signed-off-by: Sebastian (Tiedtke) Huckleberry <sebastiantiedtke@gmail.com>

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,8 +8,10 @@ import openaiLogo from './assets/openai.svg'
 import Actions from './components/Actions/Actions'
 import Chat from './components/Chat/Chat'
 import FileViewer from './components/Files/Viewer'
+import Settings from './components/Settings/Settings'
 import { AgentClientProvider } from './contexts/AgentContext'
 import { BlockProvider } from './contexts/BlockContext'
+import { SettingsProvider } from './contexts/SettingsContext'
 import Layout from './layout'
 
 function App() {
@@ -21,24 +23,36 @@ function App() {
           <meta name="description" content="An AI Assistant For Your Cloud" />
           <link rel="icon" href={openaiLogo} />
         </Helmet>
-        <AgentClientProvider>
-          <BlockProvider>
-            <BrowserRouter>
-              <Routes>
-                <Route
-                  path="/"
-                  element={
-                    <Layout
-                      left={<Chat />}
-                      middle={<Actions />}
-                      right={<FileViewer />}
-                    />
-                  }
-                />
-              </Routes>
-            </BrowserRouter>
-          </BlockProvider>
-        </AgentClientProvider>
+        <SettingsProvider>
+          <AgentClientProvider>
+            <BlockProvider>
+              <BrowserRouter>
+                <Routes>
+                  <Route
+                    path="/"
+                    element={
+                      <Layout
+                        left={<Chat />}
+                        middle={<Actions />}
+                        right={<FileViewer />}
+                      />
+                    }
+                  />
+                  <Route
+                    path="/settings"
+                    element={
+                      <Layout
+                        left={<Chat />}
+                        middle={<Actions />}
+                        right={<Settings />}
+                      />
+                    }
+                  />
+                </Routes>
+              </BrowserRouter>
+            </BlockProvider>
+          </AgentClientProvider>
+        </SettingsProvider>
       </Theme>
     </>
   )
