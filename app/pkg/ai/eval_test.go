237961b0b7a9ef880d20f2af36db8e022f76f927
zizheng <zizheng@openai.com>
2025-06-05 10:32:08 -0700

    Refactor tests in eval_test.go to use table-driven approach for assertions

--- app/pkg/ai/eval_test.go
+++ app/pkg/ai/eval_test.go
@@ -4,169 +4,163 @@ import (
 	"context"
 	"testing"
 
+	"github.com/google/go-cmp/cmp"
 	"github.com/jlewi/cloud-assistant/protos/gen/cassie"
 )
 
-func TestShellRequiredFlag_Pass(t *testing.T) {
-	asserter := shellRequiredFlag{}
-	assertion := &cassie.Assertion{
-		Name: "test-pass",
-		Type: cassie.Assertion_TYPE_SHELL_REQUIRED_FLAG,
-		Payload: &cassie.Assertion_ShellRequiredFlag_{
-			ShellRequiredFlag: &cassie.Assertion_ShellRequiredFlag{
-				Command: "kubectl",
-				Flags:   []string{"--context", "-n"},
-			},
-		},
+func TestAssertions(t *testing.T) {
+	type asserter interface {
+		Assert(ctx context.Context, assertion *cassie.Assertion, blocks map[string]*cassie.Block) error
 	}
-	blocks := map[string]*cassie.Block{
-		"1": {
-			Kind:     cassie.BlockKind_CODE,
-			Contents: "kubectl get pods --context test -n default",
-		},
-	}
-	err := asserter.Assert(context.Background(), assertion, blocks)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if assertion.Result != cassie.Assertion_RESULT_TRUE {
-		t.Errorf("expected PASSED, got %v", assertion.Result)
-	}
-}
 
-func TestShellRequiredFlag_Fail(t *testing.T) {
-	asserter := shellRequiredFlag{}
-	assertion := &cassie.Assertion{
-		Name: "test-fail",
-		Type: cassie.Assertion_TYPE_SHELL_REQUIRED_FLAG,
-		Payload: &cassie.Assertion_ShellRequiredFlag_{
-			ShellRequiredFlag: &cassie.Assertion_ShellRequiredFlag{
-				Command: "kubectl",
-				Flags:   []string{"--context", "-n"},
-			},
-		},
+	type testCase struct {
+		name           string
+		asserter       asserter
+		assertion      *cassie.Assertion
+		blocks         map[string]*cassie.Block
+		expectedResult cassie.Assertion_Result
 	}
-	blocks := map[string]*cassie.Block{
-		"1": {
-			Kind:     cassie.BlockKind_CODE,
-			Contents: "kubectl get pods --context test",
-		},
-	}
-	err := asserter.Assert(context.Background(), assertion, blocks)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if assertion.Result != cassie.Assertion_RESULT_FALSE {
-		t.Errorf("expected FAILED, got %v", assertion.Result)
-	}
-}
 
-func TestFileRetrieved_Found(t *testing.T) {
-	asserter := fileRetrieved{}
-	assertion := &cassie.Assertion{
-		Name: "file-found",
-		Type: cassie.Assertion_TYPE_FILE_RETRIEVED,
-		Payload: &cassie.Assertion_FileRetrieval_{
-			FileRetrieval: &cassie.Assertion_FileRetrieval{
-				FileId:   "file-123",
-				FileName: "test.txt",
+	testCases := []testCase{
+		{
+			name:     "kubectl-required-flags-present",
+			asserter: shellRequiredFlag{},
+			assertion: &cassie.Assertion{
+				Name: "test-pass",
+				Type: cassie.Assertion_TYPE_SHELL_REQUIRED_FLAG,
+				Payload: &cassie.Assertion_ShellRequiredFlag_{
+					ShellRequiredFlag: &cassie.Assertion_ShellRequiredFlag{
+						Command: "kubectl",
+						Flags:   []string{"--context", "-n"},
+					},
+				},
 			},
+			blocks: map[string]*cassie.Block{
+				"1": {
+					Kind:     cassie.BlockKind_CODE,
+					Contents: "kubectl get pods --context test -n default",
+				},
+			},
+			expectedResult: cassie.Assertion_RESULT_TRUE,
+		},
+		{
+			name:     "kubectl-required-flag-missing",
+			asserter: shellRequiredFlag{},
+			assertion: &cassie.Assertion{
+				Name: "test-fail",
+				Type: cassie.Assertion_TYPE_SHELL_REQUIRED_FLAG,
+				Payload: &cassie.Assertion_ShellRequiredFlag_{
+					ShellRequiredFlag: &cassie.Assertion_ShellRequiredFlag{
+						Command: "kubectl",
+						Flags:   []string{"--context", "-n"},
+					},
+				},
+			},
+			blocks: map[string]*cassie.Block{
+				"1": {
+					Kind:     cassie.BlockKind_CODE,
+					Contents: "kubectl get pods --context test",
+				},
+			},
+			expectedResult: cassie.Assertion_RESULT_FALSE,
+		},
+		{
+			name:     "file-search-file-found",
+			asserter: fileRetrieved{},
+			assertion: &cassie.Assertion{
+				Name: "file-found",
+				Type: cassie.Assertion_TYPE_FILE_RETRIEVED,
+				Payload: &cassie.Assertion_FileRetrieval_{
+					FileRetrieval: &cassie.Assertion_FileRetrieval{
+						FileId:   "file-123",
+						FileName: "test.txt",
+					},
+				},
+			},
+			blocks: map[string]*cassie.Block{
+				"block1": {
+					Kind: cassie.BlockKind_FILE_SEARCH_RESULTS,
+					FileSearchResults: []*cassie.FileSearchResult{
+						{FileID: "file-123", FileName: "test.txt"},
+					},
+				},
+			},
+			expectedResult: cassie.Assertion_RESULT_TRUE,
+		},
+		{
+			name:     "file-search-file-not-found",
+			asserter: fileRetrieved{},
+			assertion: &cassie.Assertion{
+				Name: "file-not-found",
+				Type: cassie.Assertion_TYPE_FILE_RETRIEVED,
+				Payload: &cassie.Assertion_FileRetrieval_{
+					FileRetrieval: &cassie.Assertion_FileRetrieval{
+						FileId:   "file-999",
+						FileName: "notfound.txt",
+					},
+				},
+			},
+			blocks: map[string]*cassie.Block{
+				"block1": {
+					Kind: cassie.BlockKind_FILE_SEARCH_RESULTS,
+					FileSearchResults: []*cassie.FileSearchResult{
+						{FileID: "file-123", FileName: "test.txt"},
+					},
+				},
+			},
+			expectedResult: cassie.Assertion_RESULT_FALSE,
+		},
+		{
+			name:     "tool-invocation-shell-command",
+			asserter: toolInvocation{},
+			assertion: &cassie.Assertion{
+				Name: "shell-invoked",
+				Type: cassie.Assertion_TYPE_TOOL_INVOKED,
+				Payload: &cassie.Assertion_ToolInvocation_{
+					ToolInvocation: &cassie.Assertion_ToolInvocation{
+						ToolName: "shell",
+					},
+				},
+			},
+			blocks: map[string]*cassie.Block{
+				"1": {
+					Kind:     cassie.BlockKind_CODE,
+					Contents: "echo hello world",
+				},
+			},
+			expectedResult: cassie.Assertion_RESULT_TRUE,
+		},
+		{
+			name:     "tool-invocation-no-shell-command",
+			asserter: toolInvocation{},
+			assertion: &cassie.Assertion{
+				Name: "shell-not-invoked",
+				Type: cassie.Assertion_TYPE_TOOL_INVOKED,
+				Payload: &cassie.Assertion_ToolInvocation_{
+					ToolInvocation: &cassie.Assertion_ToolInvocation{
+						ToolName: "shell",
+					},
+				},
+			},
+			blocks: map[string]*cassie.Block{
+				"1": {
+					Kind:     cassie.BlockKind_MARKUP,
+					Contents: "This is not a code block.",
+				},
+			},
+			expectedResult: cassie.Assertion_RESULT_FALSE,
 		},
 	}
-	blocks := map[string]*cassie.Block{
-		"block1": {
-			Kind: cassie.BlockKind_FILE_SEARCH_RESULTS,
-			FileSearchResults: []*cassie.FileSearchResult{
-				{FileID: "file-123", FileName: "test.txt"},
-			},
-		},
-	}
-	err := asserter.Assert(context.Background(), assertion, blocks)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if assertion.Result != cassie.Assertion_RESULT_TRUE {
-		t.Errorf("expected RESULT_TRUE, got %v", assertion.Result)
-	}
-}
 
-func TestFileRetrieved_NotFound(t *testing.T) {
-	asserter := fileRetrieved{}
-	assertion := &cassie.Assertion{
-		Name: "file-not-found",
-		Type: cassie.Assertion_TYPE_FILE_RETRIEVED,
-		Payload: &cassie.Assertion_FileRetrieval_{
-			FileRetrieval: &cassie.Assertion_FileRetrieval{
-				FileId:   "file-999",
-				FileName: "notfound.txt",
-			},
-		},
-	}
-	blocks := map[string]*cassie.Block{
-		"block1": {
-			Kind: cassie.BlockKind_FILE_SEARCH_RESULTS,
-			FileSearchResults: []*cassie.FileSearchResult{
-				{FileID: "file-123", FileName: "test.txt"},
-			},
-		},
-	}
-	err := asserter.Assert(context.Background(), assertion, blocks)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if assertion.Result != cassie.Assertion_RESULT_FALSE {
-		t.Errorf("expected RESULT_FALSE, got %v", assertion.Result)
-	}
-}
-
-func TestToolInvocation_ShellInvoked(t *testing.T) {
-	asserter := toolInvocation{}
-	assertion := &cassie.Assertion{
-		Name: "shell-invoked",
-		Type: cassie.Assertion_TYPE_TOOL_INVOKED,
-		Payload: &cassie.Assertion_ToolInvocation_{
-			ToolInvocation: &cassie.Assertion_ToolInvocation{
-				ToolName: "shell",
-			},
-		},
-	}
-	blocks := map[string]*cassie.Block{
-		"1": {
-			Kind:     cassie.BlockKind_CODE,
-			Contents: "echo hello world",
-		},
-	}
-	err := asserter.Assert(context.Background(), assertion, blocks)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if assertion.Result != cassie.Assertion_RESULT_TRUE {
-		t.Errorf("expected RESULT_TRUE, got %v", assertion.Result)
-	}
-}
-
-func TestToolInvocation_ShellNotInvoked(t *testing.T) {
-	asserter := toolInvocation{}
-	assertion := &cassie.Assertion{
-		Name: "shell-not-invoked",
-		Type: cassie.Assertion_TYPE_TOOL_INVOKED,
-		Payload: &cassie.Assertion_ToolInvocation_{
-			ToolInvocation: &cassie.Assertion_ToolInvocation{
-				ToolName: "shell",
-			},
-		},
-	}
-	blocks := map[string]*cassie.Block{
-		"1": {
-			Kind:     cassie.BlockKind_MARKUP,
-			Contents: "This is not a code block.",
-		},
-	}
-	err := asserter.Assert(context.Background(), assertion, blocks)
-	if err != nil {
-		t.Fatalf("unexpected error: %v", err)
-	}
-	if assertion.Result != cassie.Assertion_RESULT_FALSE {
-		t.Errorf("expected RESULT_FALSE, got %v", assertion.Result)
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := tc.asserter.Assert(context.TODO(), tc.assertion, tc.blocks)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if d := cmp.Diff(tc.expectedResult, tc.assertion.Result); d != "" {
+				t.Fatalf("unexpected diff in assertion result (-want +got):\n%s", d)
+			}
+		})
 	}
 }
